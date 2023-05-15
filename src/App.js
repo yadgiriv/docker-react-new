05f7317d1d85e0b02d0ce4b0b0e596c64bfacfee
Yadagiri <yadgiri123.v@gmail.com>
2023-05-15 17:41:24 +0530

    application test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hi, Suresh, Balu, Sai, Kalpana, Mamatha, Pooja, Sumana -  how are you</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
